ba02c90a58a6c1b1cf3b8695c85293ddeb5f42d3
Anthony Adebusuyi <supablast123@gmail.com>
2025-11-15 17:57:34 -0500

    Attempting to install router to link multiple pages together

---
+++ cps714-dashboard/src/Pages/ViewHome.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+
+export default function ViewHome() {
+    return (
+        <>
+            <button> View Members </button>
+            <button> Manage Members </button>
+        </>
+    )
+}
+
