cde3a49b2f16babb9493e671d8b9f0c387f7ea96
Anthony Adebusuyi <supablast123@gmail.com>
2025-11-16 14:30:30 -0500

    set up the forms to change member informatioon

--- cps714-dashboard/src/Pages/EditMember.jsx
+++ cps714-dashboard/src/Pages/EditMember.jsx
@@ -1,9 +1,38 @@
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import React, {useState, useEffect} from 'react'
 import supabase from '../supabase-client'
 
 const EditMember = () => {
     const { id } = useParams()
+    const navigate = useNavigate()
+
+    const [first_name, setFirst] = useState("")
+    const [last_name, setLast] = useState("")
+    const [email, setEmail] = useState("")
+
+    useEffect(() => {
+        const fetchMembers = async () => {
+            const { data, error } = await supabase
+            .from('members')
+            .select()
+            .eq('member_id', id)
+            .single()
+
+        if (error) {
+            navigate('/', { replace: true})
+        }
+        
+        if (data) {
+            setFirst(data.first_name)
+            setLast(data.last_name)
+            setEmail(data.email)
+            console.log(data)
+        }
+
+        }
+
+        fetchMembers()
+    }, [id, navigate]) 
 
     return (
     <div>
