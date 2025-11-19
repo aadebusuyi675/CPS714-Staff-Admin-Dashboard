b9b540e2e325510f4c5bec2ab507af0532f97b7b
Chanuth <121631184+chanuth25@users.noreply.github.com>
2025-11-19 12:16:36 -0500

    Update ViewHome.jsx

--- cps714-dashboard/src/Pages/ViewHome.jsx
+++ cps714-dashboard/src/Pages/ViewHome.jsx
@@ -6,6 +6,8 @@ export default function ViewHome() {
             <button> <Link to={"/members"}>View Members</Link></button>
             <button><Link to={"/staff"}> View Staff </Link></button>
             <button> Manage Members </button>
+            <button> <Link to={"/class"}>View Classes</Link></button>
+
         </>
     )
 }
