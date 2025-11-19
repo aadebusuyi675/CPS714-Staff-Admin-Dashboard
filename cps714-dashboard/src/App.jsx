8034ca700b8fc0ed388a339ea18a8af9e4a3dea9
Chanuth <121631184+chanuth25@users.noreply.github.com>
2025-11-19 12:21:39 -0500

    Update App.jsx

--- cps714-dashboard/src/App.jsx
+++ cps714-dashboard/src/App.jsx
@@ -2,10 +2,12 @@ import './App.css'
 import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import ViewHome from './Pages/ViewHome'
 import ViewMembers from './Pages/viewMembers'
+import ViewClass from './Pages/ViewClass'
 import Test from './Pages/Test'
 import EditMember from './Pages/EditMember'
 import ViewStaff from './Pages/ViewStaff'
 import EditStaff from './Pages/EditStaff'
+import EditClass from './Pages/EditClass'
 
 
 function App() {
@@ -19,6 +21,8 @@ function App() {
         <Route path="/editmembers/:id" element={<EditMember/>}/>
         <Route path="/editstaff/:id" element={<EditStaff/>}/>
         <Route path="/staff" element={<ViewStaff/>}/>
+        <Route path="/class" element={<ViewClass/>}/>
+        <Route path="/editclass/:id" element={<EditClass/>}/>
       </Routes>
     </Router>
   )
