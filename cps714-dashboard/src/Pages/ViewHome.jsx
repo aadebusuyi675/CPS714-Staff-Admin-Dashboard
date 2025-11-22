effaf28658eb6b74a2902be6e6c13c47d3da28ac
Anthony Adebusuyi <supablast123@gmail.com>
2025-11-22 12:24:16 -0500

    UI For Home Page and Modify page 70% complete

--- cps714-dashboard/src/Pages/ViewHome.jsx
+++ cps714-dashboard/src/Pages/ViewHome.jsx
@@ -1,14 +1,20 @@
 import { Link } from "react-router-dom"
+import '../Components/ViewHome.css'
+
 
 export default function ViewHome() {
     return (
-        <>
-            <button> <Link to={"/members"}>View Members</Link></button>
-            <button><Link to={"/staff"}> View Staff </Link></button>
-            <button> Manage Members </button>
-            <button> <Link to={"/class"}>View Classes</Link></button>
+        <div>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=id_card,person_apron,quiz" />
+            <header className="title"> FitHub - Staff Admin Dashboard </header>
+            <br/>
+            <button className="button"> <span className="material-symbols-outlined icon-setting">id_card</span> <Link to={"/members"}>View Members</Link></button>
+            <br/><br/>
+            <button className="button"> <span class="material-symbols-outlined icon-setting">person_apron</span><Link to={"/staff"}> View Staff </Link></button>
+            <br/><br/>
+            <button className="button"> <span class="material-symbols-outlined icon-setting">quiz</span> <Link to={"/class"}>View Classes</Link></button>
 
-        </>
+        </div>
     )
 }
 
