9fcf3852f172a778d3e87855ab06c3cfce345866
TMU-ChitteshC <cchandranesan@torontomu.ca>
2025-11-23 21:33:16 -0500

    Updated dashboard with ViewReport

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import ViewHome from './Pages/ViewHome'
+import ViewMembers from './Pages/viewMembers'
+import ViewClass from './Pages/ViewClass'
+import Test from './Pages/Test'
+import EditMember from './Pages/EditMember'
+import ViewStaff from './Pages/ViewStaff'
+import EditStaff from './Pages/EditStaff'
+import EditClass from './Pages/EditClass'
+import ViewReport from './Pages/ViewReport'
+
+function App() {
+  
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<ViewHome/>}/>
+        <Route path="/members" element={<ViewMembers/>}/>
+        <Route path="/test" element={<Test/>}/>
+        <Route path="/editmembers/:id" element={<EditMember/>}/>
+        <Route path="/editstaff/:id" element={<EditStaff/>}/>
+        <Route path="/staff" element={<ViewStaff/>}/>
+        <Route path="/class" element={<ViewClass/>}/>
+        <Route path="/editclass/:id" element={<EditClass/>}/>
+        <Route path="/reports" element={<ViewReport />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
