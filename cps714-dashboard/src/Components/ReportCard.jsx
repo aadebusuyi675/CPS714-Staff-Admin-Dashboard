cedabcef495beaf3ccd60d9e95b5c73e1d46ba5a
Anthony Adebusuyi <supablast123@gmail.com>
2025-11-24 00:12:30 -0500

    Update ReportCard.jsx

--- cps714-dashboard/src/Components/ReportCard.jsx
+++ cps714-dashboard/src/Components/ReportCard.jsx
@@ -1,7 +1,6 @@
 const ReportCard = ({ title, value }) => {
   return (
-    <div className="report-card">
-      <Link to="/" className="home-button">Home</Link>  
+    <div className="report-card"> 
       <h2 className="subTitle">{title}</h2>
       <p className="cardSubtitle">{value}</p>
     </div>
