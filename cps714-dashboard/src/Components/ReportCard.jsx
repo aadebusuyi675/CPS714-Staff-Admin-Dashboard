e870a7a55ea31af476c8dcc0a35f625c2ca10720
Anthony Adebusuyi <supablast123@gmail.com>
2025-11-23 23:14:27 -0500

    ui updates to report

--- cps714-dashboard/src/Components/ReportCard.jsx
+++ cps714-dashboard/src/Components/ReportCard.jsx
@@ -1,7 +1,7 @@
 const ReportCard = ({ title, value }) => {
   return (
     <div className="report-card">
-      <h2>{title}</h2>
+      <h2 className="subTitle">{title}</h2>
       <p className="cardSubtitle">{value}</p>
     </div>
   )
